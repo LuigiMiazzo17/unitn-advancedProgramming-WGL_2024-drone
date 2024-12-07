26686824f90ec31286295b70cd0d45012ca4e93d
Miazzo Luigi <17008163+LuigiMiazzo17@users.noreply.github.com>
2024-12-07 20:01:40 +0100

    test(drone): network discovery packets

--- src/drone/tests.rs
+++ src/drone/tests.rs
@@ -1,16 +1,16 @@
 use super::*;
-use crossbeam::channel::unbounded;
-use crossbeam::channel::Sender;
+use crossbeam::channel::{unbounded, Sender};
 use rand::Rng;
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::thread;
 use std::time::{Duration, Instant};
+
 use wg_2024::controller::DroneCommand;
 use wg_2024::drone::Drone;
-use wg_2024::network::SourceRoutingHeader;
-use wg_2024::packet::Ack;
-use wg_2024::packet::Fragment;
-use wg_2024::packet::{Nack, NackType, Packet, PacketType};
+use wg_2024::network::{NodeId, SourceRoutingHeader};
+use wg_2024::packet::{
+    Ack, FloodRequest, FloodResponse, Fragment, Nack, NackType, NodeType, Packet, PacketType,
+};
 use wg_2024::tests::*;
 
 fn terminate_and_assert_drone_quit(
@@ -48,48 +48,52 @@ fn generate_random_payload() -> (u8, [u8; 128]) {
 
 #[test]
 fn drone_crashes_upon_cmd() {
-    let (m_controller_send, d_controller_recv) = unbounded();
-    let (_, t_packet_recv) = unbounded();
+    let (d_command_send, d_command_recv) = unbounded();
+    let (_, d_recv) = unbounded();
 
-    let drone_t = thread::spawn(move || {
-        let mut drone = RustDrone::new(
-            1,
-            unbounded().0,
-            d_controller_recv,
-            t_packet_recv,
-            HashMap::new(),
-            0.0,
-        );
-        drone.run();
-    });
+    let d_t = thread::Builder::new()
+        .name("drone1".to_string())
+        .spawn(move || {
+            let mut drone = RustDrone::new(
+                11,
+                unbounded().0,
+                d_command_recv,
+                d_recv,
+                HashMap::new(),
+                0.0,
+            );
+            drone.run();
+        })
+        .unwrap();
 
-    terminate_and_assert_drone_quit(drone_t, m_controller_send);
+    terminate_and_assert_drone_quit(d_t, d_command_send);
 }
 
 #[test]
 fn drone_forwards_fragment() {
-    let (update_send, update_recv) = unbounded();
-    let (t_packet_send, t_packet_recv) = unbounded();
-    let (neighbour_send, neighbour_recv) = unbounded();
+    let (d_send, d_recv) = unbounded();
+    let (d2_send, d2_recv) = unbounded();
 
-    let drone_t = thread::spawn(move || {
-        let mut drone = RustDrone::new(
-            1,
-            unbounded().0,
-            update_recv,
-            t_packet_recv,
-            HashMap::new(),
-            0.0,
-        );
-        drone.run();
-    });
+    let (d_command_send, d_command_recv) = unbounded();
 
-    update_send
-        .send(DroneCommand::AddSender(2, neighbour_send))
+    let d_t = thread::Builder::new()
+        .name("drone1".to_string())
+        .spawn(move || {
+            let mut drone = RustDrone::new(
+                11,
+                unbounded().0,
+                d_command_recv,
+                d_recv,
+                HashMap::new(),
+                0.0,
+            );
+            drone.run();
+        })
         .unwrap();
 
-    // Wait for the drone to connect to the neighbour
-    thread::sleep(Duration::from_millis(100));
+    d_command_send
+        .send(DroneCommand::AddSender(12, d2_send))
+        .unwrap();
 
     let (payload_len, payload) = generate_random_payload();
     let session_id = rand::thread_rng().gen::<u64>();
@@ -102,24 +106,25 @@ fn drone_forwards_fragment() {
             data: payload,
         }),
         routing_header: SourceRoutingHeader {
-            hops: vec![1, 2],
+            hops: vec![11, 12],
             hop_index: 0,
         },
         session_id,
     };
 
     // Send the packet to the drone
-    t_packet_send.send(sending_packet.clone()).unwrap();
-
-    let received_packet = neighbour_recv.recv().unwrap();
+    d_send.send(sending_packet.clone()).unwrap();
 
     let mut expected_packet = sending_packet;
     // The packet should have been forwarded to the neighbour
     expected_packet.routing_header.hop_index = 1;
 
-    assert_eq!(received_packet, expected_packet);
+    assert_eq!(
+        d2_recv.recv_timeout(Duration::from_secs(1)).unwrap(),
+        expected_packet
+    );
 
-    terminate_and_assert_drone_quit(drone_t, update_send);
+    terminate_and_assert_drone_quit(d_t, d_command_send);
 }
 
 #[test]
@@ -134,43 +139,51 @@ fn wrap_generic_fragment_drop() {
 
 #[test]
 fn generic_chain_fragment_drop() {
-    // Client 1 channels
     let (c_send, c_recv) = unbounded();
-    // Server 21 channels
     let (s_send, _s_recv) = unbounded();
-    // Drone 11
+
     let (d_send, d_recv) = unbounded();
-    // Drone 12
-    let (d12_send, d12_recv) = unbounded();
-    // SC - needed to not make the drone crash
+    let (d2_send, d2_recv) = unbounded();
+
     let (d_command_send, d_command_recv) = unbounded();
     let (d2_command_send, d2_command_recv) = unbounded();
 
-    // Drone 11
-    let mut drone = RustDrone::new(
-        11,
-        unbounded().0,
-        d_command_recv,
-        d_recv,
-        HashMap::new(),
-        0.0,
-    );
-    // Drone 12
-    let mut drone2 = RustDrone::new(
-        12,
-        unbounded().0,
-        d2_command_recv,
-        d12_recv,
-        HashMap::new(),
-        1.0,
-    );
+    let d_t = thread::Builder::new()
+        .name("drone1".to_string())
+        .spawn(move || {
+            let mut drone = RustDrone::new(
+                11,
+                unbounded().0,
+                d_command_recv,
+                d_recv,
+                HashMap::new(),
+                0.0,
+            );
+            drone.run();
+        })
+        .unwrap();
+
+    let d2_t = thread::Builder::new()
+        .name("drone1".to_string())
+        .spawn(move || {
+            let mut drone = RustDrone::new(
+                12,
+                unbounded().0,
+                d2_command_recv,
+                d2_recv,
+                HashMap::new(),
+                1.0,
+            );
+            drone.run();
+        })
+        .unwrap();
 
     d_command_send
         .send(DroneCommand::AddSender(1, c_send.clone()))
         .unwrap();
 
     d_command_send
-        .send(DroneCommand::AddSender(12, d12_send.clone()))
+        .send(DroneCommand::AddSender(12, d2_send.clone()))
         .unwrap();
 
     d2_command_send
@@ -179,21 +192,6 @@ fn generic_chain_fragment_drop() {
 
     d2_command_send
         .send(DroneCommand::AddSender(21, s_send.clone()))
-        .unwrap();
-
-    // Spawn the drone's run method in a separate thread
-    let d_t = thread::Builder::new()
-        .name("drone".to_string())
-        .spawn(move || {
-            drone.run();
-        })
-        .unwrap();
-
-    let d2_t = thread::Builder::new()
-        .name("drone2".to_string())
-        .spawn(move || {
-            drone2.run();
-        })
         .unwrap();
 
     let (payload_size, payload) = generate_random_payload();
@@ -212,12 +210,10 @@ fn generic_chain_fragment_drop() {
         session_id: 1,
     };
 
-    // "Client" sends packet to the drone
     d_send.send(msg.clone()).unwrap();
 
-    // Client receive an NACK originated from 'd2'
     assert_eq!(
-        c_recv.recv().unwrap(),
+        c_recv.recv_timeout(Duration::from_secs(1)).unwrap(),
         Packet {
             pack_type: PacketType::Nack(Nack {
                 fragment_index: 1,
@@ -236,38 +232,45 @@ fn generic_chain_fragment_drop() {
 }
 
 #[test]
-fn test_round_trip_message() {
-    // Client<1> channels
+fn round_trip_message() {
     let (c_send, c_recv) = unbounded();
-    // Server<21> channels
     let (s_send, s_recv) = unbounded();
-    // Drone 11
+
     let (d_send, d_recv) = unbounded();
-    // Drone 12
     let (d12_send, d12_recv) = unbounded();
-    // SC for d11
+
     let (d_command_send, d_command_recv) = unbounded();
-    // SC for d12
     let (d2_command_send, d2_command_recv) = unbounded();
 
-    // Drone 11
-    let mut drone = RustDrone::new(
-        11,
-        unbounded().0,
-        d_command_recv,
-        d_recv,
-        HashMap::new(),
-        0.0,
-    );
-    // Drone 12
-    let mut drone2 = RustDrone::new(
-        12,
-        unbounded().0,
-        d2_command_recv,
-        d12_recv,
-        HashMap::new(),
-        0.0,
-    );
+    let d_t = thread::Builder::new()
+        .name("drone1".to_string())
+        .spawn(move || {
+            let mut drone = RustDrone::new(
+                11,
+                unbounded().0,
+                d_command_recv,
+                d_recv,
+                HashMap::new(),
+                0.0,
+            );
+            drone.run();
+        })
+        .unwrap();
+
+    let d2_t = thread::Builder::new()
+        .name("drone2".to_string())
+        .spawn(move || {
+            let mut drone2 = RustDrone::new(
+                12,
+                unbounded().0,
+                d2_command_recv,
+                d12_recv,
+                HashMap::new(),
+                0.0,
+            );
+            drone2.run();
+        })
+        .unwrap();
 
     d_command_send
         .send(DroneCommand::AddSender(1, c_send.clone()))
@@ -283,21 +286,6 @@ fn test_round_trip_message() {
 
     d2_command_send
         .send(DroneCommand::AddSender(11, d_send.clone()))
-        .unwrap();
-
-    // Spawn the drone's run method in a separate thread
-    let d_t = thread::Builder::new()
-        .name("drone1".to_string())
-        .spawn(move || {
-            drone.run();
-        })
-        .unwrap();
-
-    let d2_t = thread::Builder::new()
-        .name("drone2".to_string())
-        .spawn(move || {
-            drone2.run();
-        })
         .unwrap();
 
     let (payload_size, payload) = generate_random_payload();
@@ -321,7 +309,7 @@ fn test_round_trip_message() {
 
     msg.routing_header.hop_index = 3;
     // Server receives the fragment
-    assert_eq!(s_recv.recv().unwrap(), msg);
+    assert_eq!(s_recv.recv_timeout(Duration::from_secs(1)).unwrap(), msg);
 
     let mut ack = Packet {
         pack_type: PacketType::Ack(Ack { fragment_index: 0 }),
@@ -337,8 +325,313 @@ fn test_round_trip_message() {
 
     ack.routing_header.hop_index = 3;
     // Client receives the ack
-    assert_eq!(c_recv.recv().unwrap(), ack);
+    assert_eq!(c_recv.recv_timeout(Duration::from_secs(1)).unwrap(), ack);
 
     terminate_and_assert_drone_quit(d_t, d_command_send);
     terminate_and_assert_drone_quit(d2_t, d2_command_send);
+}
+
+#[test]
+fn return_flood_response_with_one_neighbour() {
+    let (c_send, c_recv) = unbounded();
+
+    let (d_send, d_recv) = unbounded();
+    let (d2_send, d2_recv) = unbounded();
+
+    let (d_command_send, d_command_recv) = unbounded();
+    let (d2_command_send, d2_command_recv) = unbounded();
+
+    let d_t = thread::Builder::new()
+        .name("drone1".to_string())
+        .spawn(move || {
+            let mut drone = RustDrone::new(
+                11,
+                unbounded().0,
+                d_command_recv,
+                d_recv,
+                HashMap::new(),
+                0.0,
+            );
+            drone.run();
+        })
+        .unwrap();
+
+    let d2_t = thread::Builder::new()
+        .name("drone2".to_string())
+        .spawn(move || {
+            let mut drone2 = RustDrone::new(
+                12,
+                unbounded().0,
+                d2_command_recv,
+                d2_recv,
+                HashMap::new(),
+                0.0,
+            );
+            drone2.run();
+        })
+        .unwrap();
+
+    d_command_send
+        .send(DroneCommand::AddSender(12, d2_send.clone()))
+        .unwrap();
+
+    d_command_send
+        .send(DroneCommand::AddSender(1, c_send.clone()))
+        .unwrap();
+
+    d2_command_send
+        .send(DroneCommand::AddSender(11, d_send.clone()))
+        .unwrap();
+
+    let session_id = rand::thread_rng().gen::<u64>();
+    let flood_id = rand::thread_rng().gen::<u64>();
+
+    let sending_flood_request = Packet {
+        pack_type: PacketType::FloodRequest(FloodRequest {
+            flood_id,
+            initiator_id: 1,
+            path_trace: vec![(1, NodeType::Client)],
+        }),
+        routing_header: SourceRoutingHeader {
+            hops: Vec::new(),
+            hop_index: 0,
+        },
+        session_id,
+    };
+
+    // Send the packet to the drone
+    d_send.send(sending_flood_request).unwrap();
+
+    let expected_packet = Packet {
+        pack_type: PacketType::FloodResponse(FloodResponse {
+            flood_id,
+            path_trace: vec![
+                (1, NodeType::Client),
+                (11, NodeType::Drone),
+                (12, NodeType::Drone),
+            ],
+        }),
+        routing_header: SourceRoutingHeader {
+            hops: vec![12, 11, 1],
+            hop_index: 2,
+        },
+        session_id,
+    };
+
+    assert_eq!(
+        c_recv.recv_timeout(Duration::from_secs(1)).unwrap(),
+        expected_packet
+    );
+
+    terminate_and_assert_drone_quit(d_t, d_command_send);
+    terminate_and_assert_drone_quit(d2_t, d2_command_send);
+}
+
+#[test]
+fn multiple_flood_requests_encounter() {
+    let (c_send, c_recv) = unbounded();
+
+    let (d_send, d_recv) = unbounded();
+    let (d2_send, d2_recv) = unbounded();
+    let (d3_send, d3_recv) = unbounded();
+    let (d4_send, d4_recv) = unbounded();
+
+    let (d_command_send, d_command_recv) = unbounded();
+    let (d2_command_send, d2_command_recv) = unbounded();
+    let (d3_command_send, d3_command_recv) = unbounded();
+    let (d4_command_send, d4_command_recv) = unbounded();
+
+    let d_t = thread::Builder::new()
+        .name("drone1".to_string())
+        .spawn(move || {
+            let mut drone = RustDrone::new(
+                11,
+                unbounded().0,
+                d_command_recv,
+                d_recv,
+                HashMap::new(),
+                0.0,
+            );
+            drone.run();
+        })
+        .unwrap();
+
+    let d2_t = thread::Builder::new()
+        .name("drone2".to_string())
+        .spawn(move || {
+            let mut drone2 = RustDrone::new(
+                12,
+                unbounded().0,
+                d2_command_recv,
+                d2_recv,
+                HashMap::new(),
+                0.0,
+            );
+            drone2.run();
+        })
+        .unwrap();
+
+    let d3_t = thread::Builder::new()
+        .name("drone3".to_string())
+        .spawn(move || {
+            let mut drone3 = RustDrone::new(
+                13,
+                unbounded().0,
+                d3_command_recv,
+                d3_recv,
+                HashMap::new(),
+                0.0,
+            );
+            drone3.run();
+        })
+        .unwrap();
+
+    let d4_t = thread::Builder::new()
+        .name("drone4".to_string())
+        .spawn(move || {
+            let mut drone4 = RustDrone::new(
+                14,
+                unbounded().0,
+                d4_command_recv,
+                d4_recv,
+                HashMap::new(),
+                0.0,
+            );
+            drone4.run();
+        })
+        .unwrap();
+
+    d_command_send
+        .send(DroneCommand::AddSender(1, c_send.clone()))
+        .unwrap();
+
+    d_command_send
+        .send(DroneCommand::AddSender(12, d2_send.clone()))
+        .unwrap();
+
+    d_command_send
+        .send(DroneCommand::AddSender(13, d3_send.clone()))
+        .unwrap();
+
+    d2_command_send
+        .send(DroneCommand::AddSender(11, d_send.clone()))
+        .unwrap();
+
+    d2_command_send
+        .send(DroneCommand::AddSender(14, d4_send.clone()))
+        .unwrap();
+
+    d3_command_send
+        .send(DroneCommand::AddSender(11, d_send.clone()))
+        .unwrap();
+
+    d3_command_send
+        .send(DroneCommand::AddSender(14, d4_send.clone()))
+        .unwrap();
+
+    d4_command_send
+        .send(DroneCommand::AddSender(12, d2_send.clone()))
+        .unwrap();
+
+    d4_command_send
+        .send(DroneCommand::AddSender(13, d3_send.clone()))
+        .unwrap();
+
+    let session_id = rand::thread_rng().gen::<u64>();
+    let flood_id = rand::thread_rng().gen::<u64>();
+
+    let sending_flood_request = Packet {
+        pack_type: PacketType::FloodRequest(FloodRequest {
+            flood_id,
+            initiator_id: 1,
+            path_trace: vec![(1, NodeType::Client)],
+        }),
+        routing_header: SourceRoutingHeader {
+            hops: Vec::new(),
+            hop_index: 0,
+        },
+        session_id,
+    };
+
+    // Send the packet to the drone
+    d_send.send(sending_flood_request).unwrap();
+
+    let mut expected_network_config = HashMap::new();
+
+    expected_network_config.insert(1, vec![(11, NodeType::Drone)]);
+    expected_network_config.insert(
+        11,
+        vec![
+            (12, NodeType::Drone),
+            (13, NodeType::Drone),
+            (1, NodeType::Client),
+        ],
+    );
+    expected_network_config.insert(12, vec![(11, NodeType::Drone), (14, NodeType::Drone)]);
+    expected_network_config.insert(13, vec![(11, NodeType::Drone), (14, NodeType::Drone)]);
+    expected_network_config.insert(14, vec![(12, NodeType::Drone), (13, NodeType::Drone)]);
+
+    fn insert_hop(
+        network_config: &mut HashMap<NodeId, Vec<(NodeId, NodeType)>>,
+        node: NodeId,
+        hop: (NodeId, NodeType),
+    ) {
+        if let Some(hops) = network_config.get_mut(&node) {
+            if !hops.contains(&hop) {
+                hops.push(hop);
+            }
+        } else {
+            network_config.insert(node, vec![hop]);
+        }
+    }
+
+    let mut flood_responses = Vec::new();
+
+    while let Ok(packet) = c_recv.recv_timeout(Duration::from_secs(1)) {
+        flood_responses.push(packet);
+    }
+
+    let mut received_network_config = HashMap::new();
+
+    assert_eq!(flood_responses.len(), 2);
+
+    for packet in flood_responses {
+        if let PacketType::FloodResponse(flood_response) = packet.pack_type {
+            assert_eq!(flood_response.flood_id, flood_id);
+
+            for (i, (hop, _)) in flood_response.path_trace.clone().into_iter().enumerate() {
+                if i != flood_response.path_trace.len() - 1 {
+                    if let Some(next_hop) = flood_response.path_trace.get(i + 1) {
+                        insert_hop(&mut received_network_config, hop, *next_hop);
+                    }
+                }
+
+                if i != 0 {
+                    if let Some(prev_hop) = flood_response.path_trace.get(i - 1) {
+                        insert_hop(&mut received_network_config, hop, *prev_hop);
+                    }
+                }
+            }
+        } else {
+            panic!("Received packet was not a FloodResponse");
+        }
+    }
+
+    for (node, hops) in expected_network_config {
+        let expected_hs = HashSet::<NodeId>::from_iter(hops.iter().map(|(id, _)| *id));
+        let received_hs = HashSet::<NodeId>::from_iter(
+            received_network_config
+                .get(&node)
+                .unwrap()
+                .iter()
+                .map(|(id, _)| *id),
+        );
+
+        assert_eq!(expected_hs, received_hs);
+    }
+
+    terminate_and_assert_drone_quit(d_t, d_command_send);
+    terminate_and_assert_drone_quit(d2_t, d2_command_send);
+    terminate_and_assert_drone_quit(d3_t, d3_command_send);
+    terminate_and_assert_drone_quit(d4_t, d4_command_send);
 }
