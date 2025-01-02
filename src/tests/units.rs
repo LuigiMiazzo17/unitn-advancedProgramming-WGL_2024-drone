e2b34335374126f1ce0ecf14ccb9f6178b8f586b
Miazzo Luigi <17008163+LuigiMiazzo17@users.noreply.github.com>
2025-01-02 01:34:58 +0100

    test(rusty_tester)

--- src/tests/units.rs
+++ src/tests/units.rs
@@ -727,3 +727,74 @@ fn flood_request_on_big_network() {
 
     terminate_env(env);
 }
+
+/*
+* "Rusty Tester" tests https://github.com/rusty-drone-2024/rusty-tester
+*/
+
+use rusty_tester::*;
+use std::time::Duration;
+
+type Tested = RustDrone;
+const TIMEOUT: Duration = Duration::from_millis(20);
+const FLOOD_TIMEOUT: Duration = Duration::from_millis(50);
+
+#[test]
+fn drone_destination_is_drone() {
+    test_drone_destination_is_drone::<Tested>(TIMEOUT);
+}
+
+#[test]
+fn drone_error_in_routing() {
+    test_drone_error_in_routing::<Tested>(TIMEOUT);
+}
+
+#[test]
+fn drone_packet_1_hop() {
+    test_drone_packet_1_hop::<Tested>(TIMEOUT);
+}
+
+#[test]
+fn drone_packet_3_hop() {
+    test_drone_packet_3_hop::<Tested>(TIMEOUT);
+}
+
+#[test]
+fn drone_packet_3_hop_crash() {
+    test_drone_packet_3_hop_crash::<Tested>(TIMEOUT);
+}
+
+#[test]
+fn easiest_flood() {
+    test_easiest_flood::<Tested>(FLOOD_TIMEOUT);
+}
+
+#[test]
+fn loop_flood() {
+    test_loop_flood::<Tested>(FLOOD_TIMEOUT);
+}
+
+#[test]
+fn hard_loop_flood() {
+    test_hard_loop_flood::<Tested>(FLOOD_TIMEOUT);
+}
+
+#[test]
+fn matrix_loop_flood() {
+    test_matrix_loop_flood::<Tested>(FLOOD_TIMEOUT);
+}
+
+#[test]
+fn star_loop_flood() {
+    test_star_loop_flood::<Tested>(FLOOD_TIMEOUT);
+}
+
+#[test]
+fn butterfly_loop_flood() {
+    test_butterfly_loop_flood::<Tested>(FLOOD_TIMEOUT);
+}
+
+#[test]
+fn tree_loop_flood() {
+    test_tree_loop_flood::<Tested>(FLOOD_TIMEOUT);
+}
